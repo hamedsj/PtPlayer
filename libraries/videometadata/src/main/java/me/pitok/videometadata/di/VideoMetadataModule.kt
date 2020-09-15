bfd0f57cfa2b0416170a5fd0060a393779d104f8
hamedsj <hamedsj5@gmail.com>
2020-09-15 15:41:39 +0430

    add videometadate module to project

---
+++ libraries/videometadata/src/main/java/me/pitok/videometadata/di/VideoMetadataModule.kt
@@ -0,0 +1,32 @@
+package me.pitok.videometadata.di
+
+import android.content.Context
+import android.media.MediaMetadataRetriever
+import coil.ImageLoader
+import coil.fetch.VideoFrameFileFetcher
+import dagger.Module
+import dagger.Provides
+import me.pitok.androidcore.qulifiers.ApplicationContext
+import me.pitok.dependencyinjection.library.LibraryScope
+
+@Module
+class VideoMetadataModule {
+
+    @Provides
+    @LibraryScope
+    fun provideCoilVideoImageLoader(@ApplicationContext context:Context): ImageLoader {
+        return ImageLoader.Builder(context).componentRegistry{
+            add(VideoFrameFileFetcher(context))
+        }
+            .build()
+    }
+
+
+    @Provides
+    @LibraryScope
+    fun provideMMDR(): MediaMetadataRetriever {
+        return MediaMetadataRetriever()
+    }
+
+
+}
