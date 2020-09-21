102844f77a2fda606acfd199876072cf70ea3094
hamedsj <hamedsj5@gmail.com>
2020-09-21 17:44:49 +0330

    add bottomSheetView from Tedu to project

---
+++ shared/design/src/main/java/me/pitok/design/views/BottomSheetViewHolder.kt
@@ -0,0 +1,29 @@
+package me.pitok.design.views
+
+import android.view.View
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.recyclerview.widget.RecyclerView
+import me.pitok.design.entity.BottomSheetItemEntity
+import kotlinx.android.synthetic.main.item_bottom_sheet.view.itemIcon
+import kotlinx.android.synthetic.main.item_bottom_sheet.view.itemTitle
+
+internal class BottomSheetViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    private val itemIcon: AppCompatImageView = view.itemIcon
+    private val itemTitle: AppCompatTextView = view.itemTitle
+
+    fun bind(item: BottomSheetItemEntity, onClick: () -> Unit) {
+        if (item.itemIconResource != null) {
+            itemIcon.setImageResource(item.itemIconResource)
+            itemIcon.visibility = View.VISIBLE
+        } else {
+            itemIcon.visibility = View.GONE
+        }
+        itemTitle.setText(item.itemTitleResource)
+        itemView.setOnClickListener {
+            onClick.invoke()
+            item.itemOnClickListener.invoke()
+        }
+    }
+}
