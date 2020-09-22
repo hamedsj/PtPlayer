9ce7bda779a5d58bc70ea5eacae986b3ccd7b9cc
hamedsj <hamedsj5@gmail.com>
2020-09-22 22:27:16 +0330

    add subtitle module

---
+++ libraries/subtitle/src/main/java/me/pitok/subtitle/SubtitleEntity.kt
@@ -0,0 +1,10 @@
+package me.pitok.subtitle
+
+data class SubtitleEntity(val content: String,
+                          val fromMs: Long,
+                          val toMs: Long,
+                          val _id: Int){
+    override fun toString(): String {
+        return "SubtitleEntity[ id: $_id \n content: $content \n from: $fromMs \n to: $toMs ]"
+    }
+}
