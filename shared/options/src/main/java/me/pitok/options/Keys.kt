8548d41591c2ed6eea0a6ae34b059a97c7e9038c
hamedsj <hamedsj5@gmail.com>
2020-10-16 16:51:19 +0330

    wip: add subtitle option to options module
    add font size reader and writer to subtitle option in options module

--- shared/options/src/main/java/me/pitok/options/Keys.kt
+++ shared/options/src/main/java/me/pitok/options/Keys.kt
@@ -4,4 +4,7 @@ object Keys {
     internal const val PLAYER_DEFAULT_SPEED_KEY = "default_playback_speed"
     internal const val PLAYER_DEFAULT_SPEAKER_VOLUME_KEY = "default_speaker_volume"
     internal const val PLAYER_DEFAULT_LAYOUT_ORIENTATION_KEY = "default_layout_orientation"
+    internal const val SUBTITLE_FONT_SIZE_KEY = "subtitle_font_size"
+    internal const val SUBTITLE_FONT_COLOR_KEY = "subtitle_font_color"
+    internal const val SUBTITLE_HIGHLIGHT_COLOR_KEY = "subtitle_highlight_color"
 }
---
+++ shared/options/src/main/java/me/pitok/options/entity/SubtitleOptionsEntity.kt
@@ -0,0 +1,5 @@
+package me.pitok.options.entity
+
+data class SubtitleOptionsEntity(val fontSize: Int?,
+                                 val fontColor: Int?,
+                                 val highlightColor: Int?)
