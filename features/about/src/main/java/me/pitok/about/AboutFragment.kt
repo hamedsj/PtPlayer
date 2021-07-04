53589ad10045d733e7993c9f47ca9d382c84c4e8
hamedsj <Hamedsj5@gmail.com>
2021-07-04 22:56:44 +0430

    use view binding instead of synthetic in `AboutFragment`

--- features/about/src/main/java/me/pitok/about/AboutFragment.kt
+++ features/about/src/main/java/me/pitok/about/AboutFragment.kt
@@ -3,34 +3,48 @@ package me.pitok.about
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
-import kotlinx.android.synthetic.main.fragment_about.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import me.pitok.about.databinding.FragmentAboutBinding
 import me.pitok.lifecycle.SingleLiveData
 import me.pitok.navigation.Navigate
 import me.pitok.navigation.observeNavigation
 
-class AboutFragment: Fragment(R.layout.fragment_about) {
+class AboutFragment : Fragment() {
 
-    companion object{
+    companion object {
         const val CLICK_ANIMATION_DURATION = 100L
     }
 
     private val navigationObservable = SingleLiveData<Navigate>()
 
+    private lateinit var binding: FragmentAboutBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAboutBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navigationObservable.observeNavigation(this@AboutFragment)
-        aboutBackIc.setOnClickListener(::onBackClickListener)
-        aboutRepoBt.setOnClickListener(::onRepoClickListener)
+        binding.aboutBackIc.setOnClickListener(::onBackClickListener)
+        binding.aboutRepoBt.setOnClickListener(::onRepoClickListener)
     }
-    private fun onBackClickListener(view: View){
-        lifecycleScope.launch{
+
+    private fun onBackClickListener(view: View) {
+        lifecycleScope.launch {
             delay(CLICK_ANIMATION_DURATION)
             withContext(Dispatchers.Main){
                 navigationObservable.value = Navigate.Up
